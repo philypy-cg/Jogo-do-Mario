d9f82727e0644af8881b922538cd647f21cb7b89
philypy-cg <119917190+philypy-cg@users.noreply.github.com>
2023-01-12 13:44:17 -0300

    quarto dia commit

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const mario = document.querySelector(".mario");
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const up = () => {
-    mario.classList.add("up");
+    mario.classList.add('up');
 
     setTimeout(() => {
-        mario.classList.remove("up");
+        mario.classList.remove('up');
 
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if (pipePosition <=120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = "none";
+        pipe.style.left ='${pipePosition}px';
+
+        mario.style.animation = "none";
+        mario.style.bottom ="${marioPosition}px";
+
+        mario.src ="/img/game-over.png";
+        mario.style.width = "75px"
+        mario.style.marginLeft = "50px"
+
+        clearInterval(loop);
+
+    }
+}, 10)
 
 document.addEventListener("keydown", up);
