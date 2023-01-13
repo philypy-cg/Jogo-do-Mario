d062f92770ca6a1f73a148db4afa8460a4722fae
philypy-cg <119917190+philypy-cg@users.noreply.github.com>
2023-01-12 23:47:13 -0300

    quarto dia auter commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const mario = document.querySelector('.mario')[0];
+const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const audioJump = document.querySelector('.audiojump');
+const gameOver = document.querySelector('.gameover');
+const textStart = document.querySelector('#text-start');
 
 const jump = () => {
-    mario.classList.add("jump");
-    setTimeout(()=>{
+    mario.classList.add('jump');
+
+    audioJump.currentTime = 0.1;
+    audioJump.volume = 0.1;
+    audioJump.play();
+
+    setTimeout(() => {
         mario.classList.remove('jump');
-    },600)
+    }, 500);
 }
 
 const loop = setInterval(() => {
@@ -13,23 +21,33 @@ const loop = setInterval(() => {
     console.log('loop')
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <=120 && pipePosition > 0 && marioPosition < 80) {
+    console.log(marioPosition);
 
-        pipe.style.animation = "none";
-        pipe.style.left ='${pipePosition}px';
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = "none";
-        mario.style.bottom ="${marioPosition}px";
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
 
-        mario.src ="game-over.png";
-        mario.style.width = "75px"
-        mario.style.marginLeft = "50px"
+        mario.src = '/imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        gameOver.currentTime = 0.1;
+        gameOver.volume = 0.2;
+        gameOver.play();
+
+        document.getElementById("text-start").style.color = "black";
+        document.getElementById("text-start").innerHTML = "<strong>GAME OVER</strong>";
 
         clearInterval(loop);
 
-    }
+    } 
+
 }, 10);
 
-document.addEventListener('keydown'), jump;
+document.addEventListener('keydown', jump);
